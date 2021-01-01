5b089938ed826834d5360d1e3cf588a8e8ff8887
m-hartzell <hartzell242@hotmail.com>
2021-01-01 12:50:46 -0500

    wip: working on displaying cap images on map

---
+++ src/components/OhioMapMarker.vue
@@ -0,0 +1,32 @@
+<template>
+  <ellipse
+    :id="position.id"
+    :key="position.id"
+    :cx="position.cx"
+    :cy="position.cy"
+    :rx="position.rx"
+    :ry="position.ry"
+    :class="[
+      'fill-current text-white z-10',
+      {
+        'text-gray-600': cap,
+        shadow: isSelected,
+      },
+    ]"
+    stroke="white"
+    :stroke-width="isSelected ? 2 : 0"
+  />
+</template>
+
+<script lang="ts">
+  import { defineComponent } from "vue";
+
+  export default defineComponent({
+    props: ["position", "cap", "isSelected"],
+    setup() {
+      return {};
+    },
+  });
+</script>
+
+<style scoped></style>
